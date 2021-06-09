0c3cbd9b8212761593079949570db3fed92dbe0d
Ekin <ekindenizkoseoglu@gmail.com>
2021-06-09 12:17:10 +0300

    Update UserValidator.cs

--- Business/ValidationRules/FluentValidation/UserValidator.cs
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -8,7 +8,7 @@ namespace Business.ValidationRules.FluentValidation
         public UserValidator()
         {
             RuleFor(u => u.Email).NotEmpty();
-            RuleFor(u => u.Email).Must(Contain).WithMessage("Your Mail adress must Contain '@' character");
+            RuleFor(u => u.Email).Must(Contain).WithMessage("Your Mail address must Contain '@' character");
             RuleFor(u => u.Email).EmailAddress();
             RuleFor(u => u.FirstName).NotEmpty();
             RuleFor(u => u.FirstName).MinimumLength(2);
@@ -20,7 +20,7 @@ namespace Business.ValidationRules.FluentValidation
             RuleFor(u => u.UserPassword).Matches(@"[0-9]+")
                 .WithMessage("Your Password must contain at least one number. (1-9)");
             RuleFor(u => u.UserPassword).Matches(@"[@\!\?\*\.]+")
-                .WithMessage("Your Password must contain at least one special character. (!, ?, *, .)");
+                .WithMessage("Your Password must contain at least one special character. (@, !, ?, *, .)");
 
         }
 

270d12af783b579be4fb2d4295073665e2138187
Ekin Köseoğlu <ekindenizkoseoglu@gmail.com>
2022-01-06 16:22:20 +0300

    Session 14 End

--- Backbone/CrossCuttingConcerns/Caching/ICacheManager.cs
+++ Backbone/CrossCuttingConcerns/Caching/ICacheManager.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
+using Microsoft.Data.SqlClient;
 
 namespace Backbone.CrossCuttingConcerns.Caching
 {
     public interface ICacheManager
     {
+        SqlTransaction BeginTransaction();
+       TransactionScope
+
         T Get<T>(string key);
         object Get(string key);
         void Add(string key, object value,int duration);
